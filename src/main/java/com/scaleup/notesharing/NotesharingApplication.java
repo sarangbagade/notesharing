6f6b27fc75ebbc8310165597522542345199d237
sarangbagade <bagadesarang@gmail.com>
2020-05-25 18:47:55 +0530

    first commit

---
+++ src/main/java/com/scaleup/notesharing/NotesharingApplication.java
@@ -0,0 +1,13 @@
+package com.scaleup.notesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesharingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaleup/notesharing/NotesharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaleup.notesharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
