f261fde332cb50e0a96768e5ad30835481a639eb
ganesh.patil9096 <ganesh.patil9096@yahoo.in>
2020-09-28 08:33:51 +0530

    change in security config

--- src/main/java/com/scaleup/notesharing/config/WebSecurityConfig.java
+++ src/main/java/com/scaleup/notesharing/config/WebSecurityConfig.java
@@ -4,15 +4,19 @@ import com.scaleup.notesharing.auth.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -22,13 +26,37 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception
     {
         http.csrf().disable();
-        http
-                .authorizeRequests()
-                .antMatchers("/dev_test/*").permitAll()
-                .antMatchers("/api_check/*").permitAll()
-                .anyRequest().authenticated()
-                .and().formLogin().permitAll()
-                .and().logout().permitAll();
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/dev_test/*").permitAll()
+//                .antMatchers("/api_check/*").permitAll()
+//                .anyRequest().authenticated()
+//                .and().formLogin().permitAll()
+//                .and().logout().permitAll();
+        http.csrf().disable()
+		// dont authenticate this particular request
+		.authorizeRequests().antMatchers("/gui/v1/**",
+				"/",
+				"/login",
+				"/authenticate",
+				"/register",
+				"/home",
+				"/resources/**",
+				"/favicon.ico").
+		permitAll().
+		antMatchers(HttpMethod.OPTIONS, "/**")
+		.permitAll().
+	
+		// all other requests need to be authenticated
+		anyRequest().authenticated().and().
+		  logout().logoutUrl("/logout").logoutSuccessUrl("/login").deleteCookies("auth_code", "JSESSIONID").invalidateHttpSession(true)
+//		  .and().
+		// make sure we use stateless session; session won't be used to
+		// store user's state.
+		//exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
+//		 .sessionManagement()
+//		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		;
     }
 
     @Bean
--- src/main/java/com/scaleup/notesharing/controller/BookController.java
+++ src/main/java/com/scaleup/notesharing/controller/BookController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api_check")
+@RequestMapping(value = "/api/v1")
 public class BookController {
 
     @Autowired
